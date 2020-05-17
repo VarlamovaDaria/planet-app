54053ee7c9d36cebde2d5861c9f618fa75e48491
daria <varlamovadaria21@gmail.com>
2020-05-17 23:56:30 +0200

    added routing, planets component and service

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PlanetsComponent } from './planets/planets.component';
+
+
+const routes: Routes = [
+  { path: '', component: PlanetsComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
