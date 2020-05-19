3d9142137d1410032ac0c3e15a539071c98a1643
daria <varlamovadaria21@gmail.com>
2020-05-19 22:56:08 +0200

    added new service

--- src/app/services/people.service.ts
+++ src/app/services/people.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { GlobalConfig } from "../configuration/global-config";
+import { PeopleObject } from "../objects/people-object";
+import {PlanetObject} from "../objects/planet-object";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PeopleService {
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient,
+    private config: GlobalConfig,
+    private people: PeopleObject ) {
+
+  }
+
+  getAllPeople() {
+    const url = '/people/';
+
+    return this.httpClient
+      .get<PeopleObject[]>(`${this.config.apiUrl}${url}`);
+  }
+
+  getPeople() {
+    return this.httpClient.get<PeopleObject[]>(`${this.people.url}`);
+  }
+
 }
