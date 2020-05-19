f868fb062c4e7311d12b8381d2134823cd29e271
daria <varlamovadaria21@gmail.com>
2020-05-19 18:35:24 +0200

    added new objects

---
+++ src/app/objects/people-object.spec.ts
@@ -0,0 +1,7 @@
+import { PeopleObject } from './people-object';
+
+describe('PeopleObject', () => {
+  it('should create an instance', () => {
+    expect(new PeopleObject()).toBeTruthy();
+  });
+});
---
+++ src/app/objects/planet-object.spec.ts
@@ -0,0 +1,7 @@
+import { PlanetObject } from './planet-object';
+
+describe('PlanetObject', () => {
+  it('should create an instance', () => {
+    expect(new PlanetObject()).toBeTruthy();
+  });
+});
